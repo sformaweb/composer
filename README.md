ccfb826bcd9f1977130de1e261a86a8067962d1d
sformaweb <sara.formaweb@gmail.com>
2021-12-14 14:50:36 +0100

    14/12/21

---
+++ README.md
@@ -0,0 +1,7 @@
+Notas e consideracións:
+
+* Creación dun sitio web con Symfony e introdución da autenticación.
+
+* Seguiuse a seguiuse esta guía https://auth0.com/blog/creating-your-first-symfony-app-and-adding-authentication/#Setting-up-the-Controllers para a realización do exercicio.
+
+  
--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,7 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Http\HttplugBundle\HttplugBundle::class => ['all' => true],
+    HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true],
 ];
